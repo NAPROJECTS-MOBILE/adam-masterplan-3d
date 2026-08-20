e0ffd636b836a31c47fd26089ffe901872b01de3
NAPROJECTS-MOBILE <nafinnshop@gmail.com>
2026-08-20 10:26:16 +0100

    Bust cache for v5.8 clearance offsets

--- calibrate/glow-bootstrap.js
+++ calibrate/glow-bootstrap.js
@@ -53,7 +53,7 @@ THREE.Box3.prototype.getSize = function(target) {
 };
 
 try {
-  await import('./app-v2.js?v=cal-56-20260820-0956');
+  await import('./app-v2.js?v=cal-58-20260820-1025');
 } finally {
   THREE.Box3.prototype.setFromObject = originalSetFromObject;
   THREE.Box3.prototype.getSize = originalGetSize;
